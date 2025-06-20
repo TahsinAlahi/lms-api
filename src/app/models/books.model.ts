e60de3006499e61cd6bdf88fdbe26fb91116d72e
Tahsin Alahi <alahitahsin@gmail.com>
2025-06-20 19:45:46 +0600

    feat: add books model

---
+++ src/app/models/books.model.ts
@@ -0,0 +1,39 @@
+import { InferSchemaType, model, Schema } from "mongoose";
+
+export enum Genre {
+  FICTION = "FICTION",
+  NON_FICTION = "NON_FICTION",
+  SCIENCE = "SCIENCE",
+  HISTORY = "HISTORY",
+  BIOGRAPHY = "BIOGRAPHY",
+  FANTASY = "FANTASY",
+}
+
+const bookSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    genre: {
+      type: String,
+      enum: Object.values(Genre),
+      required: true,
+    },
+    isbn: { type: String, required: true, unique: true },
+    description: { type: String },
+    copies: {
+      type: Number,
+      required: true,
+      min: [0, "Copies must be a non-negative number"],
+    },
+    available: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
+
+bookSchema.methods.updateAvailability = function () {
+  this.available = this.copies > 0;
+};
+
+export type IBook = InferSchemaType<typeof bookSchema>;
+
+export default model<IBook>("Book", bookSchema);
